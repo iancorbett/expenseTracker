c7b5753ce896152460bc8d44c3f3a00a607cf9a3
iancorbett <iancorbett1324@gmail.com>
2025-09-23 00:12:51 -0700

    create new pdo

--- app/db.php
+++ app/db.php
@@ -1,6 +1,6 @@
 
 <?php
-$dsn = 'sqlite:' . __DIR__ . '/../data/app.db';
+$dsn = 'sqlite:' . __DIR__ . '/../data/app.db'; //Data Source Name
 
 $options = [
     //PDO = PHP Data Objects, built-in PHP class for talking to databases
@@ -9,4 +9,8 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //queries return associative arrays
     PDO::ATTR_EMULATE_PREPARES   => false, //use real prepared statements, prevent sql injection
   ];
+  
+  $pdo = new PDO($dsn, null, null, $options); //Data Source Name, username for DB auth, password for DB auth, options array
+  //SQLite is a file-based DB and doesn’t require a username/password
+
   
