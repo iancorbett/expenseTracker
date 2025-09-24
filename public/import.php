6d5b816539767c426205f0fc0ae3ffd82867d88d
iancorbett <iancorbett1324@gmail.com>
2025-09-23 23:36:13 -0700

    create import csv function, catching errors, and sanity checks mainly

--- public/import.php
+++ public/import.php
@@ -10,3 +10,9 @@ function parse_amount_to_cents(string $amount): int { //takes in a string, retur
     if (!preg_match('/^\d+(\.\d{1,2})?$/', trim($amount))) throw new RuntimeException('Invalid amount'); //use regex validation to make sure amount entry is what we are looking for
     return (int)round((float)$amount * 100); // * 100 => converts to cents, then round value to get correct value, then convert to int
   }
+
+  function import_csv(PDO $pdo, int $uid, string $path): array { //PDO DB connection, the user ID to tie each expense to, the file path of the CSV file to import
+    if (!is_file($path)) throw new RuntimeException("CSV not found: $path"); //return error if no file found at that path
+    $fh = fopen($path, 'r'); //Opens the file for reading
+    if (!$fh) throw new RuntimeException('Cannot open CSV'); //catch error if it cant be opened
+  };  
