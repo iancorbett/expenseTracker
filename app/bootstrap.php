94aa4754066026c035d91070888bf254c69135f9
iancorbett <iancorbett1324@gmail.com>
2025-09-23 22:18:18 -0700

    require csrf directory

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,6 @@ session_start(); // track logged-in users across requests
 date_default_timezone_set('America/Los_Angeles'); //just learned san jose maps to costa rica not san jose, ca. so using LA instead
 
 require __DIR__ . '/db.php'; //db connection
+require __DIR__ . '/csrf.php';
 
-
-function e($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); } //prevents cross server scripting, converts special characters to html text
+function e($s){ return htmlspecialchars((string)$s, ENT_QUOTES, 'UTF-8'); } //prevents cross site scripting, converts special characters to html text
