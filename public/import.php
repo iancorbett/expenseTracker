a8a201fc348423e474131659fa144815cb572c14
iancorbett <iancorbett1324@gmail.com>
2025-09-23 22:36:13 -0700

    store user id and run execute functioin

---
+++ public/import.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../app/bootstrap.php';
+
+$pdo->prepare("INSERT IGNORE INTO users (id, email, password_hash) VALUES (1,'demo@example.com','demo')")->execute();
+$_SESSION['uid'] = 1; //Stores the user id in the session
+$uid = 1;
